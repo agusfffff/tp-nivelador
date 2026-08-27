e4e893faafe31bda3d52b4b26994241bc92cba99
Maria Agustina Fontana <106780131+agusfffff@users.noreply.github.com>
2026-08-27 17:53:19 -0300

    tolerate short read and short write

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -2,21 +2,45 @@ package safe_socket
 
 import "io"
 
-//TODO: Complete with a short-read/short-write tolerant implementation
-
 func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+	total := 0
+
+	for {
+		n, err := socket.Write(bytes[total:])
+
+		if err != nil {
+			return err
+		}
+
+		total += n
+
+		if total == len(bytes) {
+			return nil
+		}
 	}
-	return nil
+
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
 	buff := make([]byte, size)
-	n, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
+	total := 0
+	for {
+		n, err := socket.Read(buff[total:])
+
+		if err != nil && err != io.EOF {
+			return buff, err
+		}
+
+		if err == io.EOF && n == 0 {
+			return buff, nil
+		}
+
+		total += n
+
+		if total == size {
+			return buff, nil
+		}
+
 	}
-	return buff[:n], nil
+
 }
