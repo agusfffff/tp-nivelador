37837e798f566392fab0a15afa2728f39995e305
Maria Agustina Fontana <106780131+agusfffff@users.noreply.github.com>
2026-08-28 14:38:48 -0300

    server short read and write

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -4,8 +4,30 @@ import socket
 
 
 def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
+    total = 0 
+    buff = bytearray(size)
+    while total < size:
+        try:
+            n = socket.recv(size - total)
+        except socket.error:
+            raise RuntimeError("Socket connection closed")
+
+        buff[total:total+len(n)] = n
+
+        if len(n) == 0:
+            raise RuntimeError("Socket connection closed")
+
+        total += len(n)
+    return buff
 
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+    total = 0 
+    while total < len(bytes):
+        try:
+            n = socket.send(bytes[total:])
+        except socket.error:
+            raise RuntimeError("Socket connection closed")
+
+        total += n
+    return total
