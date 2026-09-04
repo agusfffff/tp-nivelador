ad0eb7c1510272edf47993be02c6bb9446016f0c
Maria Agustina Fontana <106780131+agusfffff@users.noreply.github.com>
2026-09-04 11:08:32 -0300

    add default AGENCY_QUORUM_MIN

--- services/server/src/main.py
+++ services/server/src/main.py
@@ -6,7 +6,7 @@ import server
 
 SERVER_HOST = os.environ["SERVER_HOST"]
 SERVER_PORT = int(os.environ["SERVER_PORT"])
-AGENCY_QUORUM_MIN = int(os.environ["AGENCY_QUORUM_MIN"])
+AGENCY_QUORUM_MIN = int(os.environ.get("AGENCY_QUORUM_MIN", 1))
 
 def main():
     logger.init()
